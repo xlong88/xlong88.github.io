0c6956465951752d416a8e3d24c1309ee76a1678
gonglong <mr.gonglong@outlook.com>
2016-03-02 10:12:19 -0500

    new post

--- assets/js/video_display.js
+++ assets/js/video_display.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
     var filename = $(".video").text();
-    $(".video").parent().append('<video width="720" height="450" controls><source src="' + filename + '" type="video/mp4"></video>');
+    $(".video").parent().append('<video width="100%" controls><source src="' + filename + '" type="video/mp4"></video>');
 	$(".video").remove();
 });
